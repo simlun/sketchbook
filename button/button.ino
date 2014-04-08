cc97789f990bd6e8adf49f0f89950879f3a9ef77
Simon Lundmark <simon.lundmark@gmail.com>
2014-04-08 21:24:40 +0200

    button: Remove need for physical pullup resistor and clean up code

--- button/button.ino
+++ button/button.ino
@@ -1,14 +1,19 @@
 const int BUTTON = 2;
 const int LED =  13;
 
+void pullUp(int pin) {
+  digitalWrite(pin, HIGH);
+}
+
 void setup() {
   pinMode(LED, OUTPUT);
-  pinMode(BUTTON, INPUT);     
+  pinMode(BUTTON, INPUT);
+  pullUp(BUTTON);  
 }
 
 void loop() {
-  int buttonState = digitalRead(BUTTON);
-  if (buttonState == LOW) {     
+  boolean buttonPressed = digitalRead(BUTTON) == LOW;
+  if (buttonPressed) {     
     digitalWrite(LED, HIGH);  
   } else {
     digitalWrite(LED, LOW); 
