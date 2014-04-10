d04025722702925e580004059b6109b713e7579a
Simon Lundmark <simon.lundmark@gmail.com>
2014-04-10 22:24:46 +0200

    analog_input: Add serial printing of analog value

--- analog_input/analog_input.ino
+++ analog_input/analog_input.ino
@@ -4,17 +4,24 @@ int analogInputPin = 0;
 int ledPins[] = {2, 3, 4, 5, 6, 7, 8, 9};
 
 void setup() {
+  Serial.begin(9600);
   for (int i = 0; i < count(ledPins); i++) {
     pinMode(ledPins[i], OUTPUT);
   }
 }
 
 void loop() {
-  int analogValue = (analogRead(analogInputPin) + 2) / 128;
+  int analogValue = analogRead(analogInputPin);
+  Serial.print("A0=");
+  Serial.println(analogValue);
+  
   for (int i = 0; i < count(ledPins); i++) {
     digitalWrite(ledPins[i], LOW);
   }
-  for (int i = 0; i < analogValue; i++) {
+  
+  int analogTreshold = (analogValue + 2) / 128;
+  for (int i = 0; i < analogTreshold; i++) {
     digitalWrite(ledPins[i], HIGH);
   }
+  delay(20);
 }
